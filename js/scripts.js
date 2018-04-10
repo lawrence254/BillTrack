04bac32f218d358cbc1cb4392bae8ba9158b1598
gracekashe <gracekashe@gmail.com>
2018-04-10 10:52:10 +0300

    re-added everthing there was from before

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//function for media query of topnav to work//
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
