1ed9edea9e1f53b78a244777b4b918885a3b7d4f
lawrence254 <lawrenslava1@gmail.com>
2018-04-12 14:50:52 +0300

    Try to resolve an issue with the upload.html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,6 @@
 // <<<<<<< HEAD
 // //funtion for media query of topnav to work//
 // =======
-<<<<<<< HEAD
-=======
-// >>>>>>> 52fcb30660c5f7b889efad8bdcfc84d83bbfb666
-
->>>>>>> 7f3d055fbd640b873aff17637d6d63b4d221a640
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
