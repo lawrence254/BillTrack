b808cd42498e29d9d50da1f0ad4d410c35adc9af
lawrence254 <lawrenslava1@gmail.com>
2018-04-12 15:53:23 +0300

    Align the cards vertically on the homepage. Remove unused class in css. Align names in README License section

--- index.php
+++ index.php
@@ -47,12 +47,13 @@ $resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($c
     <h2 class="text-center">Recent Bills Proposed in Parliament :</h2>
     <br>
       <div class="row">
+        <div class="col-md-4">
         <?php while( $record = mysqli_fetch_assoc($resultset) ) {
           $loadID = $record['id'];
 
     $fileLink = $record['file'];
-    echo "<div class='card-group col-4 col-sm-12 ' id = 'card-size'>";
-    echo "<div class='card dataCard'>";
+    echo "<div class='card-group' id='card-size'>";
+    echo "<div class='card'>";
     echo "<img class='card-img-top img-responsive' src='img/bill-tracker.jpg' alt='Leading'>";
     echo "<div class='card-body'>";
     echo "<h4 class='card-title'>".$record['proposer'];"</h4>";
@@ -70,6 +71,7 @@ $resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($c
     echo "</div>";
     ?>
     <?php } ?>
+  </div>
     </div>
     <a href="current_bills.php" class="btn">View More</a>
   </div>
